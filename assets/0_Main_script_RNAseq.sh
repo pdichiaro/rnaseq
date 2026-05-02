9f60b0b13062e953f6b3aed845f77e153a30d78c
Pierluigi Di Chiaro <pierluigi.dichiaro@ausl.re.it>
2026-05-02 17:37:36 +0200

    Update sample file and output directory for RNAseq

--- assets/0_Main_script_RNAseq.sh
+++ assets/0_Main_script_RNAseq.sh
@@ -15,11 +15,11 @@ work_dir=/mnt/ngs_ricerca/NEXTFLOW/nextflow_temp/
 conf_file=/mnt/ngs_ricerca/NEXTFLOW/local.config
 
 ######## PARAMS #########
-sample_file=/mnt/ngs_ricerca/dichiarop/Master_scripts/202505_Mesothelioma_OMIM_study_2/samplesheet.csv
-outdir=/mnt/ngs_ricerca/dichiarop/Data/202505_Mesothelioma_OMIM_study_2/
+sample_file=/mnt/ngs_ricerca/dichiarop/Master_scripts/BulkRNAseq_test/samplesheet.csv
+outdir=/mnt/ngs_ricerca/dichiarop/Data/BulkRNAseq_test/
 star_index=$UsefulData/reference_genome/STAR_index/star.v2.7.11b_gencode.v47_hg38/
 index=$UsefulData/reference_transcriptome/kallisto_index/kallisto.v0.48_gencode.v47_hg38_k31/kallisto  
-GTF=$UsefulData/reference_genome/gencode.v47.basic.annotation.gtf    #gencode.v47.primary_assembly.annotation.gtf   gencode.v47.basic.annotation.gtf
+GTF=$UsefulData/reference_genome/gencode.v47.basic.annotation.gtf 
 bed_file=$UsefulData/reference_genome/gencode.v47.bed
 genome_fasta=$UsefulData/reference_genome/GRCh38.primary_assembly.genome.fa
 transcriptome_fasta=$UsefulData/reference_transcriptome/Gencode_v47_GRCh38_p14/gencode.v47.transcripts.fa
@@ -36,10 +36,6 @@ cd $work_dir_project
 
 
 ###--- Run the script ---###
-### NOTE ###
-#--email pierluigi.dichiaro@ausl.re.it \   #if MTA (Mail Transfer Agent) is installed
-#--genome GRCh38  \  #GRCh38 --> NCBI  #hg38 --> UCSC   ## if do not you have genome .fasta (not raccomended)
-#--extra_star_align_args '--outFilterMultimapNmax 20' \
 conda activate nextflow
 
 export NXF_ASSETS=/mnt/ngs_ricerca/NEXTFLOW/Nextflow_pipeline
@@ -94,9 +90,11 @@ NXF_VER=25.04.7 nextflow run pdichiaro/rnaseq -r main \
 if [ $? -eq 0 ]; then
     echo "Nextflow finished successfully"
     scp -r {./report*,./timeline*,./trace*,./flowchart*} $outdir
-    rm -rf $work_dir_project
+    chmod -R 777 $work_dir_project 
+    #rm -rf $work_dir_project 
 else
     echo "Nextflow encountered an error"
 fi
 
 conda deactivate
+
